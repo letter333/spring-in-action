ded012d7a9766621918c7ebfde34152549d6e010
letter333 <seryong12@gmail.com>
2023-02-23 01:19:34 +0900

    챕터1

---
+++ src/test/java/sia/tacocloud/tacos/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package sia.tacocloud.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk()) // 응답이 HTTP 200이어야 한다.
+                .andExpect(view().name("home")) // home뷰가 있어야 한다.
+                .andExpect(content().string( // 콘텐츠에 "Welcome to..."가 포함되어야 한다.
+                        containsString("Welcome to...")
+                ));
+    }
+}
